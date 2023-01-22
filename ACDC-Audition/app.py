2de927a5fef03cb27bba3db6aa4165fce9b54d10
Steven Fraser <121995723+SteveJFraser@users.noreply.github.com>
2023-01-23 00:12:39 +1100

    Add files via upload
    Initial commit

---
+++ ACDC-Audition/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/loggedin')
+def loggedin():
+    first_name = request.args.get('fname-data')
+    last_name = request.args.get('lname-data')
+    return render_template('loggedin.html', first_name=first_name, last_name=last_name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
